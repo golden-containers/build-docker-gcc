7d0d2f5332cfbeee41d9940192d6422426d8a9d1
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 13:45:58 -0800

    gcc initial

--- build.sh
+++ build.sh
@@ -7,17 +7,17 @@ cd working
 
 # Checkout upstream
 
-# git clone --depth 1 --branch main https://github.com/upstream/upstream.git
-# cd upstream
+git clone --depth 1 --branch master https://github.com/docker-library/gcc
+cd gcc
 
 # Transform
 
-# sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/bullseye/" 17/bullseye/Dockerfile
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" 11/Dockerfile
 
 # Build
 
-# docker build --tag ghcr.io/golden-containers/upstream-bullseye-slim:17 17/bullseye-slim
+docker build --tag ghcr.io/golden-containers/gcc:11 11/
 
 # Push
 
-# docker push ghcr.io/golden-containers/upstream-bullseye-slim:17
+docker push ghcr.io/golden-containers/gcc -a
