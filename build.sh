5cd9b2cf193898a58a7f2129f3d2157e502f6b2e
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 17:39:08 -0800

    Default value for label

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ cd gcc
 # Transform
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" 11/Dockerfile
-echo "LABEL $1" >> 11/Dockerfile
+echo "LABEL ${1:-DEBUG=TRUE}" >> 11/Dockerfile
 
 # Build
 
