49261176ae2abc19c5defe10930c1856aa02f49c
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 12:57:53 -0800

    Refactor

--- build.sh
+++ build.sh
@@ -15,12 +15,21 @@ cd gcc
 # This sed syntax is GNU sed specific
 [ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
 
-sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" 11/Dockerfile
+${GNU_SED} -i \
+    -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" \
+    -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" \
+    11/Dockerfile
 
 # Build
 
-docker build 11/ --platform linux/amd64 --tag ghcr.io/golden-containers/gcc:11 --label ${1:-DEBUG=TRUE}
+[ -z "${1:-}" ] && BUILD_LABEL_ARG="" || BUILD_LABEL_ARG=" --label \"${1}\" "
+
+BUILD_PLATFORM=" --platform linux/amd64 "
+GCI_URL="ghcr.io/golden-containers"
+BUILD_ARGS=" ${BUILD_LABEL_ARG} ${BUILD_PLATFORM} "
+
+docker build 11/ --tag ${GCI_URL}/gcc:11 ${BUILD_ARGS}
 
 # Push
 
-docker push ghcr.io/golden-containers/gcc -a
+docker push ${GCI_URL}/gcc -a
