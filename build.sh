3d0e972434a44bb66fca7e9a0ea757c114616bd6
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 11:15:00 -0800

    Add gsed for macOS

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@ cd gcc
 
 # Transform
 
+# This sed syntax is GNU sed specific
+[ -z $(command -v gsed) ] && GNU_SED=sed || GNU_SED=gsed
+
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps\:bullseye/" 11/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> 11/Dockerfile
 
 # Build
 
